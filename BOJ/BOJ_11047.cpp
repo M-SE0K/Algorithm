3d8216deaa67a60f13a269ccad7025d1022db60e
seg7577 <112915308+seg7577@users.noreply.github.com>
2025-01-19 15:54:51 +0900

    2025-01-19

--- BOJ/BOJ_11047.cpp
+++ BOJ/BOJ_11047.cpp
@@ -1,27 +1,34 @@
-// #include <iostream>
-// #include <vector>
-// #include <algorithm>
+/*
+ 준규가 가지고 있는 동전은 총 N 종류이다. 동전을 적절히 사용해서 그 가치의 합을 K로 만들려고한다. 이때 필요한 동전 개수의 최
+ 솟값을 구하는 프로그램을 작성하시오.
+ 입력은 동전의 가치 Ai가 오름차순으로 주어지며 Ai는 Ai-1의 배수이다.
+*/
 
-// using namespace std;
+#include <iostream>
 
-// int main() {
-// 	int n, k, ans = 0;
-// 	cin >> n >> k;
+using namespace std;
 
-// 	vector<int> v(n);
-// 	for (int i = 0; i < n; i++)
-// 		cin >> v[i];
+int coinValue[10];
 
-// 	sort(v.begin(), v.end(), greater<>());
-
-// 	for (int i = 0; i < n; i++)
-// 		while (k - v[i] >= 0)
-// 		{
-// 			ans++;
-// 			k -= v[i];
-// 		}
-
-// 	cout << ans << '\n';
-
-// 	return 0;
-// }
+int main()
+{
+    int n, value;
+    cin >> n >> value;
+    
+    for (int i = 0; i < n; i++)
+        cin >> coinValue[i];
+    
+    int cnt = 0, i = n - 1;
+    while(value != 0)
+    {
+        if (coinValue[i] <= value)
+        {
+            value = value - coinValue[i];
+            cnt++;
+        }
+        else
+            i--;
+    }
+    
+    cout << cnt;
+}
