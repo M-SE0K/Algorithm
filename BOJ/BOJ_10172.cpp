3120bde8e11cb436b9a37ee14df4ca8be532d483
M-SE0K <112915308+M-SE0K@users.noreply.github.com>
2025-01-28 18:45:18 +0900

    Update BOJ_10172.cpp

--- BOJ/BOJ_10172.cpp
+++ BOJ/BOJ_10172.cpp
@@ -1,9 +1,9 @@
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     cout<<"|\\_/|"<<endl;
-//     cout<<"|q p|   /}"<<endl;
-//     cout<<"( 0 )\"\"\"\\"<<endl;
-//     cout<<"|\"^\"`    |"<<endl;
-//     cout<<"||_/=\\\\__|"<<endl;
-// }
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"|\\_/|"<<endl;
+    cout<<"|q p|   /}"<<endl;
+    cout<<"( 0 )\"\"\"\\"<<endl;
+    cout<<"|\"^\"`    |"<<endl;
+    cout<<"||_/=\\\\__|"<<endl;
+}
