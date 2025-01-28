994e13c1068013087a6ec6a4cdede674bc182417
M-SE0K <112915308+M-SE0K@users.noreply.github.com>
2025-01-28 18:49:20 +0900

    Update BOJ_1259.cpp

--- BOJ/BOJ_1259.cpp
+++ BOJ/BOJ_1259.cpp
@@ -1,32 +1,32 @@
-// #include <iostream>
-// #include <cstring>
-// #include <algorithm>
-// using namespace std;
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
 
-// int main()
-// {
-// 	ios_base::sync_with_stdio(false);
-// 	cin.tie(0);
-// 	cout.tie(0);
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
-// 	while (1)
-// 	{
-// 		string str;
-// 		cin >> str;
+	while (1)
+	{
+		string str;
+		cin >> str;
 
-// 		if (str == "0")
-// 			break;
+		if (str == "0")
+			break;
 		
-// 		string temp = str;
-// 		bool flag = false;
-// 		reverse(str.begin(), str.end());
-// 		for (int i = 0; i < temp.length(); i++)
-// 			if (temp[i] != str[i])
-// 				flag = true;
-// 		if (flag)
-// 			cout << "no" << '\n';
-// 		else
-// 			cout << "yes" << '\n';
-// 	}
-// 	return 0;
-// }
+		string temp = str;
+		bool flag = false;
+		reverse(str.begin(), str.end());
+		for (int i = 0; i < temp.length(); i++)
+			if (temp[i] != str[i])
+				flag = true;
+		if (flag)
+			cout << "no" << '\n';
+		else
+			cout << "yes" << '\n';
+	}
+	return 0;
+}
