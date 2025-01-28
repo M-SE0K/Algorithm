b1f35b89202dda007c92b55c16c4a1b4137c7806
M-SE0K <112915308+M-SE0K@users.noreply.github.com>
2025-01-28 18:47:35 +0900

    Update BOJ_11050.cpp

--- BOJ/BOJ_11050.cpp
+++ BOJ/BOJ_11050.cpp
@@ -1,24 +1,24 @@
-// #include <iostream>
+#include <iostream>
 
-// using namespace std;
+using namespace std;
 
-// int combi(int n, int k)
-// {
-// 	if (n == k || k == 0)
-// 		return 1;
-// 	return combi(n - 1, k) + combi(n - 1, k - 1);
-// }
+int combi(int n, int k)
+{
+	if (n == k || k == 0)
+		return 1;
+	return combi(n - 1, k) + combi(n - 1, k - 1);
+}
 
-// int main()
-// {
-// 	ios_base::sync_with_stdio(false);
-// 	cin.tie(0);
-// 	cout.tie(0);
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
-// 	int n, k;
-// 	cin >> n >> k;
+	int n, k;
+	cin >> n >> k;
 
-// 	cout << combi(n, k);
+	cout << combi(n, k);
 
-// 	return 0;
-// }
+	return 0;
+}
