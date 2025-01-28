dee7c29962844a3b884a6dcfce12deadcc891009
M-SE0K <112915308+M-SE0K@users.noreply.github.com>
2025-01-28 18:46:08 +0900

    Update BOJ_10809.cpp

--- BOJ/BOJ_10809.cpp
+++ BOJ/BOJ_10809.cpp
@@ -1,28 +1,28 @@
-// #include <iostream>
-// #include <string>
+#include <iostream>
+#include <string>
 
-// using namespace std;
+using namespace std;
 
 
-// int main()
-// {
-// 	string str;
-// 	cin >> str;
+int main()
+{
+	string str;
+	cin >> str;
 
-// 	int arry[125];
-// 	for (int i = 97; i <= 122; i++)
-// 		arry[i] = -1;
+	int arry[125];
+	for (int i = 97; i <= 122; i++)
+		arry[i] = -1;
 
-// 	for (int i = 0; i < str.length(); i++)
-// 		for (int j = 97; j <= 122; j++)
-// 			if (str[i] == j && arry[j] == -1)
-// 			{
-// 				arry[j] = i;
-// 				break;
-// 			}
+	for (int i = 0; i < str.length(); i++)
+		for (int j = 97; j <= 122; j++)
+			if (str[i] == j && arry[j] == -1)
+			{
+				arry[j] = i;
+				break;
+			}
 
-// 	for (int i = 97; i <= 122; i++)
-// 		cout << arry[i] << ' ';
+	for (int i = 97; i <= 122; i++)
+		cout << arry[i] << ' ';
 
-// 	return 0;
-// }
+	return 0;
+}
