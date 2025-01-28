c992887849dc898a0093eb9fb3b81394c16432c2
M-SE0K <112915308+M-SE0K@users.noreply.github.com>
2025-01-28 18:49:10 +0900

    Update BOJ_11931.cpp

--- BOJ/BOJ_11931.cpp
+++ BOJ/BOJ_11931.cpp
@@ -1,29 +1,29 @@
-// #include <iostream>
-// #include <algorithm>
-// #include <vector>
-// using namespace std;
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
 
-// vector<int>vec;
+vector<int>vec;
 
-// int main()
-// {
-// 	ios_base::sync_with_stdio(false);
-// 	cin.tie(0);
-// 	cout.tie(0);
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
-// 	int n;
-// 	cin >> n;
+	int n;
+	cin >> n;
 
-// 	for (int i = 0; i < n; i++)
-// 	{
-// 		int num;
-// 		cin >> num;
+	for (int i = 0; i < n; i++)
+	{
+		int num;
+		cin >> num;
 
-// 		vec.push_back(num);
-// 	}
-// 	sort(vec.begin(), vec.end(), greater<>());
+		vec.push_back(num);
+	}
+	sort(vec.begin(), vec.end(), greater<>());
 	
-// 	for (int i = 0; i < n; i++)
-// 		cout << vec[i] << '\n';
-// 	return 0;
-// }
+	for (int i = 0; i < n; i++)
+		cout << vec[i] << '\n';
+	return 0;
+}
