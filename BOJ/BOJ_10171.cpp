a1a400d667ef2d2f57249776f90a68403e9670e4
M-SE0K <112915308+M-SE0K@users.noreply.github.com>
2025-01-28 18:45:09 +0900

    Update BOJ_10171.cpp

--- BOJ/BOJ_10171.cpp
+++ BOJ/BOJ_10171.cpp
@@ -1,9 +1,9 @@
-// #include <iostream>
-// using namespace std;
-// int main(int argc, char const* argv[]) {
-//     cout << "\\    /\\\n";
-//     cout << " )  ( ')\n";
-//     cout << "(  /  )\n";
-//     cout << " \\(__)|\n";
-//     return 0;
-// }
+#include <iostream>
+using namespace std;
+int main(int argc, char const* argv[]) {
+    cout << "\\    /\\\n";
+    cout << " )  ( ')\n";
+    cout << "(  /  )\n";
+    cout << " \\(__)|\n";
+    return 0;
+}
