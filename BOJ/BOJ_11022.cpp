2218aff74df49092e3edcbca84f19feb82f163a7
M-SE0K <112915308+M-SE0K@users.noreply.github.com>
2025-01-28 18:47:23 +0900

    Update BOJ_11022.cpp

--- BOJ/BOJ_11022.cpp
+++ BOJ/BOJ_11022.cpp
@@ -1,5 +1,5 @@
-// t = int(input())
+t = int(input())
 
-// for i in range(t):
-//     a, b = map(int, input().split())
-//     print("Case #"+ str(i+1)+ ":", a, "+", b, "=", a+b)
+for i in range(t):
+    a, b = map(int, input().split())
+    print("Case #"+ str(i+1)+ ":", a, "+", b, "=", a+b)
