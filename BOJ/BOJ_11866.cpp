677c9904433c8355734099d9c35f80c3ef8d209f
M-SE0K <112915308+M-SE0K@users.noreply.github.com>
2025-01-28 18:48:56 +0900

    Update BOJ_11866.cpp

--- BOJ/BOJ_11866.cpp
+++ BOJ/BOJ_11866.cpp
@@ -1,35 +1,35 @@
-// #include <iostream>
-// #include <queue>
+#include <iostream>
+#include <queue>
 
-// using namespace std;
+using namespace std;
 
-// queue<int> q;
-// int main()
-// {
-// 	int n, k;
-// 	cin >> n >> k;
+queue<int> q;
+int main()
+{
+	int n, k;
+	cin >> n >> k;
 
-// 	for (int i = 1; i <= n; i++)
-// 		q.push(i);
-// 	int cnt = 1;
-// 	cout << "<";
-// 	while (!q.empty())
-// 	{
-// 		if (cnt == k)
-// 		{
-// 			cout << q.front();
+	for (int i = 1; i <= n; i++)
+		q.push(i);
+	int cnt = 1;
+	cout << "<";
+	while (!q.empty())
+	{
+		if (cnt == k)
+		{
+			cout << q.front();
 			
-// 			q.pop();
-// 			if (q.size() != 0)
-// 				cout << ", ";
-// 			cnt = 1;
-// 		}
-// 		else
-// 		{
-// 			q.push(q.front());
-// 			q.pop();
-// 			cnt++;
-// 		}
-// 	}
-// 	cout << ">";
-// }
+			q.pop();
+			if (q.size() != 0)
+				cout << ", ";
+			cnt = 1;
+		}
+		else
+		{
+			q.push(q.front());
+			q.pop();
+			cnt++;
+		}
+	}
+	cout << ">";
+}
