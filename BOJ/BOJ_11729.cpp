947ac844935a98849e44f805ea0d3e8766b28995
M-SE0K <112915308+M-SE0K@users.noreply.github.com>
2025-01-28 18:48:39 +0900

    Update BOJ_11729.cpp

--- BOJ/BOJ_11729.cpp
+++ BOJ/BOJ_11729.cpp
@@ -1,33 +1,33 @@
-// #include <iostream>
+#include <iostream>
 
-// using namespace std;
+using namespace std;
 
-// void Hanoi(int n, int start, int to, int end);
+void Hanoi(int n, int start, int to, int end);
 
-// int main()
-// {
-// 	ios::sync_with_stdio(false);
-// 	cin.tie(0);
-// 	cout.tie(0);
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
-// 	int n;
-// 	cin >> n;
+	int n;
+	cin >> n;
 
-// 	cout << (1 << n) - 1<< '\n';
-// 	Hanoi(n, 1, 3, 2);
+	cout << (1 << n) - 1<< '\n';
+	Hanoi(n, 1, 3, 2);
 
-// 	return 0;
-// }
+	return 0;
+}
 
 
-// void Hanoi(int n, int start, int to, int end)
-// {
-// 	if (n == 1)
-// 		cout << start << " " << to << '\n';
-// 	else
-// 	{
-// 		Hanoi(n - 1, start, end, to);
-// 		cout << start << " " << to << '\n';
-// 		Hanoi(n - 1, end, to, start);
-// 	}
-// }
+void Hanoi(int n, int start, int to, int end)
+{
+	if (n == 1)
+		cout << start << " " << to << '\n';
+	else
+	{
+		Hanoi(n - 1, start, end, to);
+		cout << start << " " << to << '\n';
+		Hanoi(n - 1, end, to, start);
+	}
+}
