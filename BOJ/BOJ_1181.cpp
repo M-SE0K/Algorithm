89821f62c2af3991938192b0f863794bc23fb9e1
M-SE0K <112915308+M-SE0K@users.noreply.github.com>
2025-01-28 18:48:47 +0900

    Update BOJ_1181.cpp

--- BOJ/BOJ_1181.cpp
+++ BOJ/BOJ_1181.cpp
@@ -1,44 +1,44 @@
-// #include <iostream>
-// #include <vector>
-// #include <cstring>
-// #include <algorithm>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
 
-// using namespace std;
+using namespace std;
 
-// vector <string> vec;
-// bool cmp(const string& a, const string& b)
-// {
-// 	if (a.length() == b.length())
-// 		return a < b;
-// 	return a.length() < b.length();
-// }
-// int main()
-// {
-// 	ios_base::sync_with_stdio(false);
-// 	cin.tie(0);
-// 	cout.tie(0);
+vector <string> vec;
+bool cmp(const string& a, const string& b)
+{
+	if (a.length() == b.length())
+		return a < b;
+	return a.length() < b.length();
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
-// 	int n;
-// 	cin >> n;
+	int n;
+	cin >> n;
 
-// 	for (int i = 0; i < n; i++)
-// 	{
-// 		string str;
-// 		cin >> str;
+	for (int i = 0; i < n; i++)
+	{
+		string str;
+		cin >> str;
 
-// 		vec.push_back(str);
-// 	}
-// 	sort(vec.begin(), vec.end(), cmp);
+		vec.push_back(str);
+	}
+	sort(vec.begin(), vec.end(), cmp);
 
-// 	string str = "";
-// 	for (int i = 0; i < n; i++)
-// 	{
-// 		if (str != vec[i])
-// 		{
-// 			cout << vec[i] << '\n';
-// 			str = vec[i];
-// 		}
-// 	}
-// 	return 0;
+	string str = "";
+	for (int i = 0; i < n; i++)
+	{
+		if (str != vec[i])
+		{
+			cout << vec[i] << '\n';
+			str = vec[i];
+		}
+	}
+	return 0;
 		
-// }
+}
