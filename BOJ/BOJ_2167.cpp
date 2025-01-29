acb780a3f21e89c2627baccbdf717e3658358f99
M-SE0K <112915308+M-SE0K@users.noreply.github.com>
2025-01-29 22:27:48 +0900

    Update BOJ_2167.cpp

--- BOJ/BOJ_2167.cpp
+++ BOJ/BOJ_2167.cpp
@@ -1,34 +1,31 @@
-// #include <iostream>
-// //#include <string>
-// //#include <algorithm>
-// #define MAX 301
-// using namespace std;
-// //#include <string>
-// //#include <algorithm>
+#include <iostream>
+#define MAX 301
+using namespace std;
 
-// int n, m, i, j, k, x, y, num, hum = {0};
-// int arry[MAX][MAX] = {};
-// int main()
-// {
-// 	//입력 제대로 먹음
-// 	cin >> n >> m;
-// 	for (int i = 1; i <= n; i++)
-// 		for (int j = 1; j <= m; j++)
-// 			cin >> arry[i][j];
 
-// 	cin >> k;
+int n, m, i, j, k, x, y, num, hum = {0};
+int arry[MAX][MAX] = {};
+int main()
+{
+	//입력 제대로 먹음
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= m; j++)
+			cin >> arry[i][j];
+
+	cin >> k;
 	
-// 	for (int a = 1; a <= k; a++)
-// 	{
-// 		hum = 0;
-// 		cin >> i >> j >> x >> y;
-// 		for (int b = i; b <= x; b++)
-// 		{
-// 			for (int c = j; c <= y; c++)
-// 				hum += arry[b][c];
-// 		}
-// 		cout << hum << '\n';
-// 	}
+	for (int a = 1; a <= k; a++)
+	{
+		hum = 0;
+		cin >> i >> j >> x >> y;
+		for (int b = i; b <= x; b++)
+		{
+			for (int c = j; c <= y; c++)
+				hum += arry[b][c];
+		}
+		cout << hum << '\n';
+	}
 
-// 	return 0;
-// }
+	return 0;
+}
