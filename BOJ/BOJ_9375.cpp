07f5b34a3c176e80e2f153301e0b086f07439564
M-SE0K <112915308+M-SE0K@users.noreply.github.com>
2025-01-29 22:32:27 +0900

    Update BOJ_9375.cpp

--- BOJ/BOJ_9375.cpp
+++ BOJ/BOJ_9375.cpp
@@ -1,39 +1,39 @@
-// #include <iostream>
-// #include <map>
-// #include <string.h>
-// #include <vector>
+#include <iostream>
+#include <map>
+#include <string.h>
+#include <vector>
 
-// using namespace std;
+using namespace std;
 
-// int main()
-// {
-// 	ios_base::sync_with_stdio(false);
-// 	cin.tie(0);
-// 	cout.tie(0);
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
-// 	int t;
-// 	cin >> t;
+	int t;
+	cin >> t;
 
-// 	while (t--)
-// 	{
-// 		int n;
-// 		cin >> n;
+	while (t--)
+	{
+		int n;
+		cin >> n;
 
-// 		map <string, int> m;
-// 		for (int i = 0; i < n; i++)
-// 		{
-// 			string name, part;
-// 			cin >> name >> part;
+		map <string, int> m;
+		for (int i = 0; i < n; i++)
+		{
+			string name, part;
+			cin >> name >> part;
 
-// 			m[part]++;
-// 		}
+			m[part]++;
+		}
 
-// 		vector<pair<string ,int>> vec(m.begin(), m.end());
-// 		int ans = 1;
-// 		for (int i = 0; i < vec.size(); i++)
-// 			ans *= vec[i].second + 1;
-// 		cout << ans - 1 << '\n';
-// 	}
+		vector<pair<string ,int>> vec(m.begin(), m.end());
+		int ans = 1;
+		for (int i = 0; i < vec.size(); i++)
+			ans *= vec[i].second + 1;
+		cout << ans - 1 << '\n';
+	}
 
-// 	return 0;
-// }
+	return 0;
+}
