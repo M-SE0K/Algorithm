65caf448cbf4f6ae746fa9fe785303c0a0109525
M-SE0K <112915308+M-SE0K@users.noreply.github.com>
2025-01-29 22:23:46 +0900

    Update BOJ_17928.cpp

--- BOJ/BOJ_17928.cpp
+++ BOJ/BOJ_17928.cpp
@@ -1,38 +1,38 @@
-// #include <iostream>
-// #include <stack>
+#include <iostream>
+#include <stack>
 
-// using namespace std;
+using namespace std;
 
-// stack<int> s;
+stack<int> s;
 
-// int main()
-// {
-// 	ios_base::sync_with_stdio(false);
-// 	cin.tie(0);
-// 	cout.tie(0);
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
-// 	int n;
-// 	cin >> n;
+	int n;
+	cin >> n;
 
-// 	int* arry = new int[n + 1];
-// 	for (int i = 0; i < n; i++)
-// 		cin >> arry[i];
+	int* arry = new int[n + 1];
+	for (int i = 0; i < n; i++)
+		cin >> arry[i];
 
-// 	int* sol = new int[n + 1];
-// 	for (int i = n - 1; 0 <= i; i--)
-// 	{
-// 		while (!s.empty() && s.top() <= arry[i])
-// 			s.pop();
+	int* sol = new int[n + 1];
+	for (int i = n - 1; 0 <= i; i--)
+	{
+		while (!s.empty() && s.top() <= arry[i])
+			s.pop();
 
-// 		if (s.empty())
-// 			sol[i] = -1;
-// 		else
-// 			sol[i] = s.top();
-// 		s.push(arry[i]);
-// 	}
+		if (s.empty())
+			sol[i] = -1;
+		else
+			sol[i] = s.top();
+		s.push(arry[i]);
+	}
 
-// 	for (int i = 0; i < n; i++)
-// 		cout << sol[i] << ' ';
-// 	delete arry, sol;
-// 	return 0;
-// }
+	for (int i = 0; i < n; i++)
+		cout << sol[i] << ' ';
+	delete arry, sol;
+	return 0;
+}
