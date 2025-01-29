a534fafddde9dc0ca0ccdfb3e37a705d8086d4c6
M-SE0K <112915308+M-SE0K@users.noreply.github.com>
2025-01-29 22:22:54 +0900

    Update BOJ_1715.cpp

--- BOJ/BOJ_1715.cpp
+++ BOJ/BOJ_1715.cpp
@@ -1,41 +1,41 @@
-// #include <iostream>
-// #include <queue>
+#include <iostream>
+#include <queue>
 
-// using namespace std;
+using namespace std;
 
-// priority_queue <int, vector<int>, greater<int>> pq;
+priority_queue <int, vector<int>, greater<int>> pq;
 
-// int main()
-// {
-// 	int n;
-// 	cin >> n;
+int main()
+{
+	int n;
+	cin >> n;
 
-// 	if (n == 1)
-// 	{
-// 		cout << 0;
-// 		return 0;
-// 	}
+	if (n == 1)
+	{
+		cout << 0;
+		return 0;
+	}
 
-// 	for (int i = 0; i < n; i++)
-// 	{
-// 		int x;
-// 		cin >> x;
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
 
-// 		pq.push(x);
-// 	}
+		pq.push(x);
+	}
 
-// 	int ans = 0;
-// 	while (pq.size() > 1)
-// 	{
-// 		int a = pq.top();
-// 		pq.pop();
-// 		int b = pq.top();
-// 		pq.pop();
+	int ans = 0;
+	while (pq.size() > 1)
+	{
+		int a = pq.top();
+		pq.pop();
+		int b = pq.top();
+		pq.pop();
 
-// 		ans += a + b;
-// 		pq.push(a + b);
-// 	}
-// 	cout << ans;
-// 	return 0;
+		ans += a + b;
+		pq.push(a + b);
+	}
+	cout << ans;
+	return 0;
 
-// }
+}
