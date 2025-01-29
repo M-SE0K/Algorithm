f3d95d6048421d871d9c9b94f94afe65f3df58a6
M-SE0K <112915308+M-SE0K@users.noreply.github.com>
2025-01-29 22:27:23 +0900

    Update BOJ_2161.cpp

--- BOJ/BOJ_2161.cpp
+++ BOJ/BOJ_2161.cpp
@@ -1,24 +1,24 @@
-// #include <iostream>
-// #include <queue>
+#include <iostream>
+#include <queue>
 
-// using namespace std;
+using namespace std;
 
-// queue <int> q;
+queue <int> q;
 
-// int main()
-// {
-// 	int n;
-// 	cin >> n;
-// 	for (int i = 1; i <= n; i++)
-// 		q.push(i);
-// 	while (q.size() != 1)
-// 	{
-// 		cout << q.front() << ' ';
-// 		q.pop();
-// 		q.push(q.front());
-// 		q.pop();
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		q.push(i);
+	while (q.size() != 1)
+	{
+		cout << q.front() << ' ';
+		q.pop();
+		q.push(q.front());
+		q.pop();
 		
-// 	}
-// 	cout << q.front();
-// 	return 0;
-// }
+	}
+	cout << q.front();
+	return 0;
+}
