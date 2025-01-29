bbef847c9ed640fb45982cdb3f826f199b099f38
M-SE0K <112915308+M-SE0K@users.noreply.github.com>
2025-01-29 22:29:47 +0900

    Update BOJ_2751.cpp

--- BOJ/BOJ_2751.cpp
+++ BOJ/BOJ_2751.cpp
@@ -1,22 +1,22 @@
-// #include <iostream>
-// #include <algorithm>
+#include <iostream>
+#include <algorithm>
 
-// using namespace std;
+using namespace std;
 
-// int arry[1000005];
-// int main()
-// {
-// 	ios_base::sync_with_stdio(false);
-// 	cin.tie(0);
-// 	cout.tie(0);
+int arry[1000005];
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
 
-// 	int n;
-// 	cin >> n;
+	int n;
+	cin >> n;
 
-// 	for (int i = 0; i < n; i++)
-// 		cin >> arry[i];
-// 	sort(arry, arry + n);
-// 	for (int i = 0; i < n; i++)
-// 		cout << arry[i] << '\n';
-// 	return 0;
-// }
+	for (int i = 0; i < n; i++)
+		cin >> arry[i];
+	sort(arry, arry + n);
+	for (int i = 0; i < n; i++)
+		cout << arry[i] << '\n';
+	return 0;
+}
